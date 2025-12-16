8d9409865f91db8d30d493358d084b8add9c985b
Javier <javierbarrezueta1@gmail.com>
2025-12-15 21:47:04 -0500

    Ejercicio 02 - Stack Sorter en Java y Python

--- python/app.py
+++ python/app.py
@@ -1,12 +1,25 @@
 from sing_validator import SignValidator
+from stack_sorter import StackSorter
 
 def run_sign_validator():
+    print("\n--- Ejercicio 1 ---")
     signValidator  = SignValidator()
-    cadena1 = "{[()]}"; # VALIDO
-    cadena2 = "{[(])}"; # NO VALIDO
+    cadena1 = "{[()]}"
+    cadena2 = "{[(])}"
 
     print("Cadena: " + cadena1 + " es valida? " + str(signValidator.is_valid(cadena1)))
     print("Cadena: " + cadena2 + " es valida? " + str(signValidator.is_valid(cadena2)))
+
+def run_stack_sorter():
+    print("\n--- Ejercicio 2 ---")
+    sorter = StackSorter()
     
+    stack = [5, 1, 4, 2]
+    
+    print("Stack original: " + str(stack))
+    sorter.sort_stack(stack)
+    print("Stack ordenado: " + str(stack))
+
 if __name__ == "__main__":
     run_sign_validator()
+    run_stack_sorter()
--- src/App.java
+++ src/App.java
@@ -1,17 +1,49 @@
+import java.util.Stack;
+
+import utils.QueuePalindrome;
 import utils.SignValidator;
+import utils.StackSorter;
 
 public class App {
     public static void main(String[] args) throws Exception {
         SignValidator signValidator = new SignValidator();
         runSignValidator();
+
+        StackSorter stackSorter = new StackSorter();
+        runStackSorter();
+
+        
     }
 
+    
+
     private static void runSignValidator() {
+        System.out.println("Ejercicio 1");
         SignValidator signValidator = new SignValidator();
         String cadena1 = "{[()]}"; // VALIDO
         String cadena2 = "{[(])}"; // NO VALIDO
 
         System.out.println("Cadena: " + cadena1 + " es valida? " + signValidator.isValid(cadena1));
         System.out.println("Cadena: " + cadena2 + " es valida? " + signValidator.isValid(cadena2));
+        System.out.println("\n");
+    }
+
+    private static void runStackSorter() {
+        System.out.println("Ejercicio 2");
+        StackSorter sorter = new StackSorter();
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(2);
+        stack.push(4);
+        stack.push(1);
+        stack.push(5);
+
+        System.out.println("Stack original: " + stack);
+        
+        sorter.sortStack(stack);
+        
+        System.out.println("Stack ordenado: " + stack);
+        System.out.println("Tope actual: " + stack.peek());
+        System.out.println("\n");
     }
 }
